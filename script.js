593fb3542dae8f38507fdc58e598f74d22a54ab5
raffaele-catalano <raf.catalano@gmail.com>
2023-10-19 23:02:10 +0200

    added loader meanwhile elaborating solutions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.getElementById('queensForm').addEventListener('submit', function(event) {
+document.getElementById('queensForm').addEventListener('submit', function (event) {
     event.preventDefault();
+
+    // Disabilita il modulo durante l'elaborazione
+    document.getElementById('NQueens').disabled = true;
+    document.getElementById('loader').style.display = 'block';
+
     const numQueens = document.getElementById('NQueens').value;
     fetch('solver.php', {
         method: 'POST',
@@ -8,35 +13,39 @@ document.getElementById('queensForm').addEventListener('submit', function(event)
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json()) // parse la risposta JSON
-    .then(data => {
-        // elabora la risposta e visualizza le soluzioni nella tua pagina HTML
-        const solutionsContainer = document.getElementById('solutions');
-        solutionsContainer.innerHTML = ''; // Cancella eventuali soluzioni precedenti
+        .then(response => response.json()) // parse la risposta JSON
+        .then(data => {
+            // Abilita nuovamente il modulo
+            document.getElementById('NQueens').disabled = false;
+            document.getElementById('loader').style.display = 'none';
 
-        if (data.solutionsCount > 0) {
-            data.solutions.forEach(solution => {
-                const board = document.createElement('div');
-                board.className = 'board';
-                board.style.setProperty('--n', numQueens);
+            // elabora la risposta e visualizza le soluzioni nella tua pagina HTML
+            const solutionsContainer = document.getElementById('solutions');
+            solutionsContainer.innerHTML = ''; // Cancella eventuali soluzioni precedenti
 
-                solution.forEach(row => {
-                    row.forEach(cell => {
-                        const cellElement = document.createElement('div');
-                        cellElement.className = 'cell';
-                        if (cell === 1) {
-                            cellElement.innerHTML = '<i class="fa-solid fa-chess-queen"></i>'; // simbolo di una regina stilizzata
-                            cellElement.classList.add('queen');
-                        }
-                        board.appendChild(cellElement);
+            if (data.solutionsCount > 0) {
+                data.solutions.forEach(solution => {
+                    const board = document.createElement('div');
+                    board.className = 'board';
+                    board.style.setProperty('--n', numQueens);
+
+                    solution.forEach(row => {
+                        row.forEach(cell => {
+                            const cellElement = document.createElement('div');
+                            cellElement.className = 'cell';
+                            if (cell === 1) {
+                                cellElement.innerHTML = '<i class="fa-solid fa-chess-queen"></i>'; // simbolo di una regina stilizzata
+                                cellElement.classList.add('queen');
+                            }
+                            board.appendChild(cellElement);
+                        });
                     });
-                });
 
-                solutionsContainer.appendChild(board);
-            });
-        } else {
-            // Nessuna soluzione trovata
-            solutionsContainer.innerHTML = '<h3 class="mt-3 text-danger">Nessuna soluzione valida trovata per ' + numQueens + ' regine.</h3>';
-        }
-    });
+                    solutionsContainer.appendChild(board);
+                });
+            } else {
+                // Nessuna soluzione trovata
+                solutionsContainer.innerHTML = '<h3 class="mt-3 text-danger">Nessuna soluzione valida trovata per ' + numQueens + ' regine <i class="fa-solid fa-chess-queen fa-bounce text-warning"></i></h3>';
+            }
+        });
 });
